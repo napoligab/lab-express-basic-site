7eb9b0bca5df9ada427626dd0ba10425b789aa74
Gabriella Hevesy <55456609+napoligab@users.noreply.github.com>
2022-07-26 17:37:29 +0100

    solved lab without gallery

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+app.use(express.static('public'));
+
+
+app.get('/home', (request, response, next) => {
+  response.sendFile(__dirname + '/public/views/home.html');
+});
+
+app.get('/about', (request, response, next) => {
+  response.sendFile(__dirname + '/public/views/about.html');
+});
+
+app.get('/works', (request, response, next) => {
+  response.sendFile(__dirname + '/public/views/works.html');
+});
+
+app.get('/gallery', (request, response, next) => {
+  response.sendFile(__dirname + '/public/views/gallery.html');
+});
+
+
+app.listen(3000, () => {
+  console.log('Running on port 3000');
+});
